a3b97b1b5a9b78ed466caa915ebdd7b4e104d476
Saher Saadi <sahersaadi@Sahers-MacBook-Air.local>
2026-02-13 19:05:46 +0200

    Add CV PDF and update contact section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { FaGithub, FaLinkedin, FaEnvelope, FaArrowDown } from "react-icons/fa";
-import myPhoto from "../assets/saher.jpeg";
+import myPhoto from "../assets/Saher.jpeg";
 
 export function Hero() {
   return (
